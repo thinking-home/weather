f658959b62e485fd365bb1b46e317d02289552e8
postcretinism <postcretinism@gmail.com>
2024-09-23 18:55:05 +0300

    первый общий запрос сделал запрос, дождался его завершения написал хттп клиент, добавил базовый адрес и заголовки для всех запросов сделал запрос для получения погоды (амстердам) и вывел ответ в консоль

--- ThinkingHome.Weather.TestConsole/Program.cs
+++ ThinkingHome.Weather.TestConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using ThinkingHome.Weather.Api;
+﻿using System.Net.Http;
+using System.Runtime.CompilerServices;
+using ThinkingHome.Weather.Api;
 
 namespace ThinkingHome.Weather.TestConsole
 {
@@ -7,7 +9,17 @@ namespace ThinkingHome.Weather.TestConsole
         static void Main(string[] args)
         {
             YandexWeatherClient client = new YandexWeatherClient();
-            client.SayMoo();
+            var ttt = Response();
+            ttt.Wait();
+        }
+        static async Task Response() {
+            HttpClient yandexWeather = new HttpClient()
+            {
+                BaseAddress = new Uri("https://api.weather.yandex.ru/v2/"),
+            };
+            yandexWeather.DefaultRequestHeaders.Add("X-Yandex-Weather-Key", "c6e1cdc8-3f10-4583-a744-25e7eb042594");
+            string response = await yandexWeather.GetStringAsync("forecast?lat=52.37125&lon=4.89388");
+            Console.WriteLine(response);
         }
     }
 }
