2f6fb04b261c119b3133e8c420cc7a28e0247e58
postcretinism <postcretinism@gmail.com>
2024-10-06 00:21:07 +0300

    вывел поля Cloudness ObsTime Humidity закомментировал четыре поля для реализации eNum

--- ThinkingHome.Weather.Api/WeatherFactData.cs
+++ ThinkingHome.Weather.Api/WeatherFactData.cs
@@ -9,4 +9,26 @@ public class WeatherFactData {
 
     [JsonPropertyName("feels_like")]
     public int FeelsLike { get; set; }
+
+    [JsonPropertyName("obs_time")]
+    public int ObsTime { get; set; }
+
+    [JsonPropertyName("cloudness")]
+    public float Cloudness { get; set; }
+
+    [JsonPropertyName("humidity")]
+    public int Humidity { get; set; }
+
+    //[JsonPropertyName("icon")]
+    //public string Icon { get; set; }
+
+    //[JsonPropertyName("condition")]
+    //public string Condition { get; set; }
+
+    //[JsonPropertyName("season")]
+    //public string Season { get; set; }
+
+    //[JsonPropertyName("daytime")]
+    //public string Daytime { get; set; }
+
 }
