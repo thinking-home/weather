d2dba8afb11b0519bf8eb8f61209500fd9afa8a8
postcretinism <postcretinism@gmail.com>
2024-11-24 13:00:50 +0300

    сделал вывод маскированного ключа, начала запроса и уеичтожения weather client в лог

--- ThinkingHome.Weather.Api/YandexWeatherClient.cs
+++ ThinkingHome.Weather.Api/YandexWeatherClient.cs
@@ -12,17 +12,23 @@ public class YandexWeatherClient : IDisposable
     };
     
     
-    private readonly ILogger logger; 
-    
+    private readonly ILogger logger;
+
+    private string MaskApiKey(string apiKey)
+    {
+        return apiKey.Substring(apiKey.Length - 6);
+    }
+
     public YandexWeatherClient(string apiKey, ILogger logger)
     {
         yandexWeatherHttp.DefaultRequestHeaders.Add("X-Yandex-Weather-Key", apiKey);
         this.logger = logger;
-        logger.LogInformation($"Yandex Weather API Key: {apiKey}");
+        logger.LogInformation($"Yandex Weather API Key: ***{MaskApiKey(apiKey)}");
     }
 
     public void Dispose()
     {
+        logger.LogInformation("Dispose weather client");
         yandexWeatherHttp.Dispose();
     }
 
@@ -30,6 +36,8 @@ public class YandexWeatherClient : IDisposable
     {
         var slat = JsonSerializer.Serialize(lat);
         var slon = JsonSerializer.Serialize(lon);
+        
+        logger.LogInformation($"Getting forecast for {slat}, {slon}");
         var json = await yandexWeatherHttp.GetStringAsync($"forecast?lat={slat}&lon={slon}");
         var response = JsonSerializer.Deserialize<ForecastResponse>(json);
         return response;
