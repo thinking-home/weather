4ff8868a5d4d8dcb286d118b2b64a1147828bed2
postcretinism <postcretinism@gmail.com>
2024-12-18 22:50:06 +0300

    описали документацию к DayPartsWeatherData и ForecastData

--- ThinkingHome.Weather.Api/Model/DayPartsWeatherData.cs
+++ ThinkingHome.Weather.Api/Model/DayPartsWeatherData.cs
@@ -2,17 +2,37 @@ using System.Text.Json.Serialization;
 
 namespace ThinkingHome.Weather.Api.Model;
 
+/// <summary>
+/// Прогноз по времени суток
+/// </summary>
 public class DayPartsWeatherData
 {
-    [JsonPropertyName("day")] public DayPartWeatherData Day { get; set; }
-
-    [JsonPropertyName("evening")] public DayPartWeatherData Evening { get; set; }
-
+    /// <summary>
+    /// Утро (6:00-11:59)
+    /// </summary>
     [JsonPropertyName("morning")] public DayPartWeatherData Morning { get; set; }
-
+    
+    /// <summary>
+    /// День (12:00-17:00)
+    /// </summary>
+    [JsonPropertyName("day")] public DayPartWeatherData Day { get; set; }
+    /// <summary>
+    /// Вечер (18:00-21:59)
+    /// </summary>
+    [JsonPropertyName("evening")] public DayPartWeatherData Evening { get; set; }
+    
+    /// <summary>
+    /// Ночь (0:00-5:59)
+    /// </summary>
     [JsonPropertyName("night")] public DayPartWeatherData Night { get; set; }
 
+    /// <summary>
+    /// Темное время суток (0:00-5:59)
+    /// </summary>
     [JsonPropertyName("night_short")] public DayPartShortWeatherData NightShort { get; set; }
 
+    /// <summary>
+    /// Светлое время суток (6:00-21:59)
+    /// </summary>
     [JsonPropertyName("day_short")] public DayPartShortWeatherData DayShort { get; set; }
 }
--- ThinkingHome.Weather.Api/Model/ForecastData.cs
+++ ThinkingHome.Weather.Api/Model/ForecastData.cs
@@ -4,25 +4,55 @@ using ThinkingHome.Weather.Api.Model.Enum;
 
 namespace ThinkingHome.Weather.Api.Model;
 
+/// <summary>
+/// Прогноз погоды на день
+/// </summary>
 public class ForecastData
 {
+    /// <summary>
+    /// Начало дня
+    /// </summary>
     [JsonConverter(typeof(UnixDateTimeConverter))]
     [JsonPropertyName("date_ts")]
     public DateTime Date { get; set; }
 
+    /// <summary>
+    /// Номер недели в году
+    /// </summary>
     [JsonPropertyName("week")] public int Week { get; set; }
 
+    /// <summary>
+    /// Время восхода
+    /// </summary>
     [JsonPropertyName("sunrise")] public string Sunrise { get; set; }
 
+    /// <summary>
+    /// Время заката
+    /// </summary>
     [JsonPropertyName("sunset")] public string Sunset { get; set; }
 
+    /// <summary>
+    /// Время начала восхода
+    /// </summary>
     [JsonPropertyName("rise_begin")] public string RiseBegin { get; set; }
 
+    /// <summary>
+    /// Время окончания заката
+    /// </summary>
     [JsonPropertyName("set_end")] public string SetEnd { get; set; }
 
+    /// <summary>
+    /// Фаза луны
+    /// </summary>
     [JsonPropertyName("moon_code")] public MoonPhase MoonPhase { get; set; }
 
+    /// <summary>
+    /// Прогнос погоды по времени суток 
+    /// </summary>
     [JsonPropertyName("parts")] public DayPartsWeatherData Parts { get; set; }
 
+    /// <summary>
+    /// Прогноз погоды по часам
+    /// </summary>
     [JsonPropertyName("hours")] public HourWeatherData[] Hours { get; set; }
 }
